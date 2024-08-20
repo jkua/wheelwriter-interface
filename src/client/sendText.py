1f6485b2e90cd403a4be73387aec206cb8f73960
John Kua <john@kua.fm>
2024-08-19 22:24:35 -0700

    sendText: Added noPrintableEscape option to prevent trigging escape sequences when sending arbitrary ASCII text

--- src/client/sendText.py
+++ src/client/sendText.py
@@ -13,6 +13,7 @@ if __name__=='__main__':
 	parser.add_argument('file', help='Text file to send')
 	parser.add_argument('--endlines', '-e', type=int, default=0, help='Additional carriage returns at the end')
 	parser.add_argument('--keyboard', '-k', type=int, choices=[1, 103], default=1, help='Keyboard layout')
+	parser.add_argument('--noPrintableEscape', action='store_true', help='Do not allow printable escape sequence ^[, binary only')
 	args = parser.parse_args()
 
 	# with WheelwriterClient(args.device) as client:
@@ -48,8 +49,9 @@ if __name__=='__main__':
 			time.sleep(1)
 
 		print('\n*** Switching to type mode ***')
-		print(f'type {args.keyboard}\n')
-		ser.write(f'type {args.keyboard}\n'.encode())
+		commandString = f'type {args.keyboard} {int(not args.noPrintableEscape)}'
+		print(f'{commandString}\n')
+		ser.write(f'{commandString}\n'.encode())
 
 		while True:
 			line = ser.readline().decode().strip()
